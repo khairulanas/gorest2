54741716999b5fb5cde5f54624025aec517a866b
khairul anas <mafiakanan@gmail.com>
2020-11-01 19:45:13 +0700

    update sql

--- initdb.sql
+++ initdb.sql
@@ -1,9 +1,13 @@
+mysql -u root
+
 CREATE DATABASE be04;
 USE be04;
 CREATE USER `user04`@`localhost` IDENTIFIED BY '1234567';
 GRANT ALL PRIVILEGES ON be04.* TO `user04`@`localhost`;
 FLUSH PRIVILEGES; 
--- mysql -u user04 -p be04
+exit
+
+mysql -u user04 -p be04
 
 CREATE TABLE students (
 	`id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
